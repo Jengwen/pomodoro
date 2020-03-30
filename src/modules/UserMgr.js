bb21224c517c12ad2dfa0cd689f3dfc993712b53
Jengwen <jenalexsarah@gmail.com>
2020-03-30 16:14:43 -0400

    updated API calls for tasks and users

---
+++ src/modules/UserMgr.js
@@ -0,0 +1,24 @@
+//create local host variable
+const remoteURL = "http://localhost:44358/api";
+
+export default {
+    //   bring back users from database based on parameters
+getAllUsers(){
+  return fetch(`${remoteURL}/Users`).then(result => result.json());
+},
+//bring back one user from database
+getOneUser(id) {
+  return fetch(`${remoteUrl}/Users/${id}`).then(result => result.json());
+},
+//post a new user to database
+
+post(newUser) {
+    return fetch(`${remoteURL}/Users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newUser)
+    }).then(data => data.json());
+  },
+}
